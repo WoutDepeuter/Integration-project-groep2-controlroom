09ea74cc76286b23ece39c0a6fa0ad8f684e4ff5
hossam01101998 <123333030+hossam01101998@users.noreply.github.com>
2025-05-22 13:19:47 +0200

    trying to solve reconnecting problem

--- extensions/heartbeat/heartbeat_monitoring.py
+++ extensions/heartbeat/heartbeat_monitoring.py
@@ -7,11 +7,27 @@ from datetime import datetime, timedelta, UTC
 import xmltodict
 
 from elastic import get_last_heartbeats
-from env import RABBITMQ_EXCHANGE, RABBITMQ_ROUTING_KEY, RABBITMQ_CHANNEL
+from env import RABBITMQ_HOST, RABBITMQ_EXCHANGE, RABBITMQ_ROUTING_KEY, RABBITMQ_CHANNEL
+
+_connection = None
+
+def get_connection():
+    global _connection
+    try:
+        if _connection is None or _connection.is_closed:
+            logging.info("Connecting to RabbitMQ...")
+            parameters = pika.ConnectionParameters(host=RABBITMQ_HOST, heartbeat=60)
+            _connection = pika.BlockingConnection(parameters)
+        return _connection
+    except Exception as e:
+        logging.error(f"Could not connect to RabbitMQ: {e}")
+        _connection = None
+        return None
 
 previous_down_services = {}
 
-def send_alert(connection: pika.BlockingConnection, down_services: dict[str, datetime]):
+def send_alert(down_services: dict[str, datetime]):
+#def send_alert(connection: pika.BlockingConnection, down_services: dict[str, datetime]):
 
     # TODO: Save current down services, and take diff (don't need to send an email every 10s if the same stuff stays down
 #    new_down = down_services
@@ -61,6 +77,11 @@ def send_alert(connection: pika.BlockingConnection, down_services: dict[str, dat
     xml_str = xmltodict.unparse(data, pretty=True)
     logging.debug("Heartbeat mail dto \n%s", xml_str)
 
+    connection = get_connection()
+    if connection is None:
+        logging.error("No connection with RabbitMQ, It skips sending the alert")
+        return
+
     try:
         # TODO: Is it fine to open a new channel for this? Should I pass through the channel object instead?
         channel = connection.channel()
@@ -69,10 +90,24 @@ def send_alert(connection: pika.BlockingConnection, down_services: dict[str, dat
             routing_key=RABBITMQ_ROUTING_KEY,
             body=xml_str,
         )
+    except pika.exceptions.StreamLostError:
+        logging.warning("Connection lost during the sending, trying again...")
+        connection.close()
+        connection = get_connection()
+        if connection:
+            channel = connection.channel()
+            channel.basic_publish(
+                exchange=RABBITMQ_EXCHANGE,
+                routing_key=RABBITMQ_ROUTING_KEY,
+                body=xml_str,
+            )
+        else:
+            logging.error("Could not reconnect to send the message")
     except Exception:
-        logging.exception("Failed to mail, how to monitor this???????", exc_info=True)
+        logging.exception("Error sending alert")
 
-def heartbeat_loop(connection: pika.BlockingConnection):
+#def heartbeat_loop(connection: pika.BlockingConnection):
+def heartbeat_loop():
     logging.debug("Heartbeat monitor checking apps")
     heartbeats = get_last_heartbeats()
     logging.debug(f"Received heartbeats from {list(heartbeats.keys())}")
@@ -87,7 +122,8 @@ def heartbeat_loop(connection: pika.BlockingConnection):
             logging.debug(f"Received heartbeat in time: {sender}")
 
     if len(down_services) > 0:
-        send_alert(connection, down_services)
+#        send_alert(connection, down_services)
+        send_alert(down_services)
         pass
 
     time.sleep(10)
